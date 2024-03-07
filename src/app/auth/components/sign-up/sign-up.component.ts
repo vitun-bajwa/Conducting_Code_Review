00203e989ffe7e8cfa4aa94c2ec9a5a8cf83d8e8
vitun bajwa <vitunbajwawins@gmail.com>
2024-03-07 18:23:18 +0530

    changes

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -26,7 +26,7 @@ export class SignUpComponent {
         ...this.form.form.value,
         status: 'Inactive'
       }
-      this.apiService.add('user', data).subscribe((res: any) => {
+      this.apiService.add('users', data).subscribe((res: any) => {
         this.snackBar.open('sign-up successfully','',{
           duration: 1000
         });
