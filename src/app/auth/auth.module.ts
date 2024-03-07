90cc8161cce914184f662c5947be22df5a50a311
vitun bajwa <vitunbajwawins@gmail.com>
2024-03-07 15:21:59 +0530

    chnage

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
@@ -9,8 +8,6 @@ import { SharedModule } from '../shared/shared.module';
 import { NgOtpInputModule } from  'ng-otp-input';
 import { UiModule } from '../ui/ui.module';
 import { ButtonFieldComponent } from "../shared/dynmic-form/component/button-field/button-field.component";
-
-
 
 @NgModule({
     declarations: [
